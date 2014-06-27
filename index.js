dba9dd3716440543f5000e5f4916c386315d295f
mani <mani95lisa@gmail.com>
2014-06-28 01:55:47 +0800

    fix escape character bugs

--- index.js
+++ index.js
@@ -760,6 +760,16 @@
             re = /(<pre><code class="lang-swift">)(.*[\s\S]*?)(<\/code><\/pre>)/gmi
             arr.forEach(function (value) {
                 var code = value.replace(re, '$2');
+                var letter = {
+                    '&amp;' : '&',
+                    '&gt;' : '>',
+                    '&lt;' : '<',
+                    '&quot;' : '"',
+                    '&#39;' : "'"
+                };
+                code = code.replace(/(&amp;)|(&gt;)|(&lt;)|(&quot;)|(&#39;)/gmi,function($0){
+                    return letter[$0];
+                })
                 var highlightedCode = hljs.highlight('swift',code).value;
                 postContent = postContent.replace(value, '<pre><code class="hljs swift">'+highlightedCode+'</code></pre>');
             });
